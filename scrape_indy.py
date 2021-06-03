ca21d729ec26310907c328ea83dc6d4b4c1143ff
Gregory Maginn <gregorymaginn@Gregorys-Mac-mini.local>
2021-06-03 09:35:22 -0700

    all table data correctly being pulled

--- scrape_indy.py
+++ scrape_indy.py
@@ -26,11 +26,28 @@ def scraper():
     
     tHead = body[0]
     bodyRows = body[1:]
-    x = 0
-    for result in bodyRows:
-        x = x+1
-        print(x)
-        print(result.text)
+    
+    for rowNum in range(len(bodyRows)):
+        columnArray = bodyRows[rowNum].find_all('td')
+        x = 0
+        #for colNum in columnArray:
+         #   print(columnArray[x].text)
+          #  x =x+1
 
-
+        while x < len(columnArray):
+            print(columnArray[x].text)
+            x =x+1
+        #for result in bodyRows:
+         #   print(f'Finishing Position: {result.text[0]}')
+          #  print(f'Qualifying Position: {result.text[1]}')
+           # print(f'Car Number: {result.text[2]}')
+            #print(f'Driver Name: {result.text[3]}')
+            #need to split string on ()
+            #print(f'Sponsor/Owner: {result.text[4]}')
+            #need number between C/ and /F to find engine maker
+            #print(f'Engine: {result.text[5]}')
+            #print(f'Laps Completed: {result.text[6]}')
+            #print(f'Laps Lead: {result.text[8]}')
+            #print(f'Points Awarded: {result.text[9]}')
+            #print('=======================')
 scraper()
